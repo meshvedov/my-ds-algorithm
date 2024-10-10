1d7d83bac8e822e039f8ba737700766b26194fc2
MSh <micangel@mail.ru>
2024-10-10 16:25:36 +0300

    max_leafs problem

--- src/MyTreeClf.py
+++ src/MyTreeClf.py
@@ -19,6 +19,7 @@ class MyTreeClf:
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.leafs_cnt = 0
+        self.leafs_sum = 0
 
     def __str__(self) -> str:
         return f"MyTreeClf class: max_depth={self.max_depth}, min_samples_split={self.min_samples_split}, max_leafs={self.max_leafs}"
@@ -27,10 +28,12 @@ class MyTreeClf:
     def fit(self, X: pd.DataFrame, y: pd.Series):
         def is_leaf(sub: pd.Series, depth):
             return any([depth < 0,
-                        len(sub) == 1,
+                        sub.size == 1,
                         sub.value_counts().size == 1,
-                        len(sub) < self.min_samples_split,
-                        self.leafs_cnt >= self.max_leafs - 1])
+                        sub.size < self.min_samples_split,
+                        self.leafs_cnt > self.max_leafs]
+                       )
+
         def probability_one(labels: pd.Series):
             return np.mean(labels)
 
@@ -38,30 +41,29 @@ class MyTreeClf:
             depth -= 1
             if is_leaf(y, depth):
                 self.leafs_cnt += 1
-                p1_l = probability_one(y)
-                return ['leaf_left', p1_l]
+                p1 = probability_one(y)
+                self.leafs_sum += p1
+                return ['leaf_left', p1]
 
             col_name, split, ig = get_best_split2(X, y)
             left = X[X[col_name] < split]
             sub_left = ['node', col_name, split, tree_create(left.reset_index(drop=1), y[left.index].reset_index(drop=1), depth)]
 
-
             right = X[X[col_name] > split]
             if is_leaf(y[right.index], depth-1):
                 self.leafs_cnt += 1
-                p1_r = probability_one(y[right.index])
-                sub_right = ['leaf_right', p1_r]
+                p1 = probability_one(y[right.index])
+                self.leafs_sum += p1
+                sub_right = ['leaf_right', p1]
             else:
                 sub_right = tree_create(right.reset_index(drop=1), y[right.index].reset_index(drop=1), depth)
             sub_left.append(sub_right)
             return sub_left
 
         self.tree = tree_create(X, y, depth=self.max_depth)
-        pprint(self.tree, indent=4)
-        print(self.leafs_cnt)
 
     def print_tree(self):
-        pass
+        pprint(self.tree, indent=4)
 
 def get_best_split(X: pd.DataFrame, y: pd.Series):
     col_name, split_value, ig = X.columns[0], 0, 0
@@ -129,11 +131,14 @@ def get_best_split2(X: pd.DataFrame, y: pd.Series):
 
     return col_name, split_value, ig
 
-# tr = MyTreeClf(max_depth=1, min_samples_split=1, max_leafs=2)
+tr = MyTreeClf(max_depth=1, min_samples_split=1, max_leafs=2)
 # tr = MyTreeClf(max_depth=3, min_samples_split=2, max_leafs=5)
 # tr = MyTreeClf(max_depth=5, min_samples_split=200, max_leafs=10)
 # tr = MyTreeClf(max_depth=4, min_samples_split=100, max_leafs=17)
 # tr = MyTreeClf(max_depth=10, min_samples_split=40, max_leafs=21)
-tr = MyTreeClf(max_depth=15, min_samples_split=20, max_leafs=30)
+# tr = MyTreeClf(max_depth=15, min_samples_split=20, max_leafs=30)
+# tr = MyTreeClf(max_depth=3, min_samples_split=2, max_leafs=2)
 # print(get_best_split(X, y))
 tr.fit(X, y)
+pprint(tr.tree, indent=4)
+print(tr.leafs_cnt, tr.leafs_sum)
